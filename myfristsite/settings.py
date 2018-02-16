50d79af00c75c445fb39ff525ef4b620a78032cb
wr771137363 <wr695251173@163.com>
2018-02-16 20:44:09 +0800

    Update settings.py

--- myfristsite/settings.py
+++ myfristsite/settings.py
@@ -22,6 +22,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'wr695251173@gmail.com'
 EMAIL_HOST_PASSWORD = 'ILOVEYOUmywife.@'
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Rui Wang <wr695251173@gmail.com>'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
