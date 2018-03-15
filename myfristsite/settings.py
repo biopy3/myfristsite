91c4d5587f9ce29fed66fa3cb3c5dcc34f1d57d7
wr771137363 <wr695251173@163.com>
2018-03-15 18:23:19 +0800

    Update settings.py

--- myfristsite/settings.py
+++ myfristsite/settings.py
@@ -18,12 +18,12 @@ CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24
 
 #email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'wr695251173@gmail.com'
-EMAIL_HOST_PASSWORD = 'ILOVEYOUmywife.@'
+EMAIL_HOST_USER = 'wonray@outlook.com.au'
+EMAIL_HOST_PASSWORD = 'Wr771137363.@'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Rui Wang <wr695251173@gmail.com>'
+DEFAULT_FROM_EMAIL = 'Rui Wang <wonray@outlook.com.au>'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
