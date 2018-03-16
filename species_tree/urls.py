e3d92b2797f1b6eafa210b51c30e9dde9f277569
wonray <wr695251173@163.com>
2018-03-17 00:45:43 +0800

    03/17

--- species_tree/urls.py
+++ species_tree/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
+    path('result/',views.query_get_results),
     path('manual.pdf',views.document),
     path('', views.home_page),
     path('save_post/',views.save_post),
