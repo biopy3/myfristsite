1d46c47b2f88555f488db939d467ee94868b1150
wr771137363 <wr695251173@163.com>
2018-03-17 14:02:50 +0800

    Update urls.py

--- species_tree/urls.py
+++ species_tree/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
+    path('result/download/',view.get_results)
     path('result/',views.query_get_results),
     path('manual.pdf',views.document),
     path('', views.home_page),
