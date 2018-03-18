df24565d243d7bb9bbf6efba99b38699bafe33b4
wr771137363 <wr695251173@163.com>
2018-03-18 17:15:09 +0800

    Update tasks.py

--- species_tree/tasks.py
+++ species_tree/tasks.py
@@ -215,12 +215,14 @@ def generate_tree(infile_path,send_email,user_name,access_code):
         from_email = settings.DEFAULT_FROM_EMAIL
         email = EmailMessage(
             subject='Hello,' + user_name + ':',
-            body="Thank you use the SCPC web service,we send this email with results for you.Please visit the url:\n"+
-            "http://45.76.122.117:8000/home/result/download/"+access_code,
+            body='<p>Thank you use the SCPC web service,we send this email with results for you.Please visit the url:\n</p>\
+            </br><a href=http://45.76.122.117:8000/home/result/download/"+access_code>\
+            http://45.76.122.117:8000/home/result/download/' + access_code + '</a>'
+           
             from_email=from_email,
             to=[send_email]
         )
-
+        email.content_subtype = "html"  # Main content is now text/html
         #email.attach_file('' + dir_path + '.zip')
         email.send()
         conn.close()
