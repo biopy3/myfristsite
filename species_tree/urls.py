9a62a2e9a38ebd4ab88ad2a0d1989d4decd00653
wr771137363 <wr695251173@163.com>
2018-03-18 15:58:16 +0800

    Update urls.py

--- species_tree/urls.py
+++ species_tree/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('result/download/',views.download_results),
-    path('result/',views.query_get_results),
     path('manual.pdf',views.document),
     path('', views.home_page),
     path('save_post/',views.save_post),
