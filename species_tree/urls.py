15dcf8afaa937a78900e89f2d80bac6ebdf8abf3
wr771137363 <wr695251173@163.com>
2018-03-18 16:16:55 +0800

    Update urls.py

--- species_tree/urls.py
+++ species_tree/urls.py
@@ -2,7 +2,8 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    path('result/download/',views.download_results),
+    r'^plist/$'
+    path(r'^result/download/$',views.download_results),
     path('manual.pdf',views.document),
     path('', views.home_page),
     path('save_post/',views.save_post),
