13330c7f975dbcc667974a58ec38d909bd33df99
dabingbeizheng <wr695251173@163.com>
2018-04-10 14:44:25 +0800

    Update models.py

--- species_tree/models.py
+++ species_tree/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Records(models.Model):
     user = models.CharField(max_length=200)
     inputfile = models.FileField(upload_to ='species_tree/recordsfile/')
-    access_code = models.CharField(max_length=15)
+    access_code = models.CharField(max_length=15,primary_key = True,default = '')
     resultfile = models.FileField()
     email = models.EmailField()
 
 class clustalx_model(models.Model):
     user = models.CharField(max_length=200)
     inputfile = models.FileField(upload_to ='species_tree/recordsfile/')
-    access_code = models.CharField(max_length=15)
+    access_code = models.CharField(max_length=15,primary_key = True,default = '')
     output_file = models.FileField()
     email = models.EmailField()
