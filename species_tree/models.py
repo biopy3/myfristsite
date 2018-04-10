a77dc6324f4c4c4da1e3c347492b18a2f6188fd4
dabingbeizheng <wr695251173@163.com>
2018-04-10 14:36:02 +0800

    Update models.py

--- species_tree/models.py
+++ species_tree/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Records(models.Model):
     user = models.CharField(max_length=200)
     inputfile = models.FileField(upload_to ='species_tree/recordsfile/')
-    access_code = models.CharField(max_length=15,primary_key=True)
+    access_code = models.CharField(max_length=15)
     resultfile = models.FileField()
     email = models.EmailField()
 
