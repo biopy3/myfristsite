b46b6153d41e55edc22155fc0d224065fb778855
dabingbeizheng <wr695251173@163.com>
2018-04-10 14:24:54 +0800

    Update models.py

--- species_tree/models.py
+++ species_tree/models.py
@@ -9,7 +9,7 @@ class Records(models.Model):
     resultfile = models.FileField()
     email = models.EmailField()
 
-class clustalx_model():
+class clustalx_model(models.Model):
     user = models.CharField(max_length=200)
     inputfile = models.FileField(upload_to ='species_tree/recordsfile/')
     access_code = models.CharField(max_length=15,primary_key=True)
