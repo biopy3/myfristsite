b6ef74aebfafdafbcf82608af100b743d75e9c62
dabingbeizheng <wr695251173@163.com>
2018-04-24 16:51:07 +0800

    Add files via upload

--- myfristsite/settings.py
+++ myfristsite/settings.py
@@ -21,7 +21,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'wr695251173@gmail.com'
-EMAIL_HOST_PASSWORD = 'ILOVEYOUwsj.@'
+EMAIL_HOST_PASSWORD = 'ILOVEYOUmywife.@'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Rui Wang <wr695251173@gmail.com>'
 
